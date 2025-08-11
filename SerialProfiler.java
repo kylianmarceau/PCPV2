7d1cc09fa146bf54ba2bfffbbccc00d3e6cdef8a
kylianmarceau <kylianmarceau@gmail.com>
2025-08-11 17:12:41 +0200

    replaced total timer i had, with excution timers from DungeonHunter

--- SerialProfiler.java
+++ SerialProfiler.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class SerialProfiler {
     //store the values for testing in a list
-    static List<Integer> GRID_SIZES = Arrays.asList(50, 100, 150, 200);
+    static List<Integer> GRID_SIZES = Arrays.asList(100, 200, 500, 1000);
     static List<Double> DENSITIES  = Arrays.asList(0.05, 0.10, 0.20);
     static List<Integer> SEEDS = Arrays.asList(1, 2, 3);
 
@@ -25,12 +25,12 @@ public class SerialProfiler {
                     int seed = SEEDS.get(y);
 
                     //get start op time
-                    long start = System.nanoTime();
+                    //long start = System.nanoTime(); ---> dont need -- can access public static timers from DungeonHunter.java
                     DungeonHunter.main(new String[]{String.valueOf(size), String.valueOf(density), String.valueOf(seed)});
 
                     //get end system time
-                    long end = System.nanoTime();
-                    long timeTaken = (end-start)/1_000_000;
+                    //long end = System.nanoTime(); ---> dont need -- can access public static timers from DungeonHunter.java
+                    long timeTaken = DungeonHunter.endTime - DungeonHunter.startTime;
 
                     fileWriter.write(size + ", " + density + ", " + seed + ", " + timeTaken + "\n");
                     System.out.printf("Ran %d %.2f %d -> %d ms%n", size, density, seed, timeTaken);
