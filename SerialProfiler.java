edc12e32741f4c3f08a8a47024bf147872b71fc2
kylianmarceau <kylianmarceau@gmail.com>
2025-08-11 16:54:33 +0200

    implemented profiling logic

--- SerialProfiler.java
+++ SerialProfiler.java
@@ -2,10 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class SerialProfiler {
+    //store the values for testing in a list
     static List<Integer> GRID_SIZES = Arrays.asList(50, 100, 150, 200);
     static List<Double> DENSITIES  = Arrays.asList(0.05, 0.10, 0.20);
     static List<Integer> SEEDS = Arrays.asList(1, 2, 3);
-    public static void main(String[] args) {
-        
+
+    static String OUTPUT_FILE = "serial_tests_output.txt";
+    public static void main(String[] args) throws IOException {
+        profile();
+
+    }
+
+    public static void profile()throws IOException{
+        FileWriter fileWriter = new FileWriter(OUTPUT_FILE);
+        fileWriter.write("Grid_Size, Density, Seed, Time_ms\n");
+
+        for(int i = 0; i <GRID_SIZES.size(); i++){
+            int size = GRID_SIZES.get(i);
+            for(int x = 0; x<DENSITIES.size(); x++){
+                double density = DENSITIES.get(x);
+                for(int y = 0; y < SEEDS.size(); y++){
+                    int seed = SEEDS.get(y);
+
+                    //get start op time
+                    long start = System.nanoTime();
+                    DungeonHunter.main(new String[]{String.valueOf(size), String.valueOf(density), String.valueOf(seed)});
+
+                    //get end system time
+                    long end = System.nanoTime();
+                    long timeTaken = (end-start)/1_000_000;
+
+                    fileWriter.write(size + ", " + density + ", " + seed + ", " + timeTaken + "\n");
+                    System.out.printf("Ran %d %.2f %d -> %d ms%n", size, density, seed, timeTaken);
+                }
+            }
+        }
+        System.out.println("Profiling complete. Results saved to " + OUTPUT_FILE);
+
+        fileWriter.close();
     }
 }
