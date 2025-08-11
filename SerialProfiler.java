b09d569be1f5dda68584299ac16866da550076b1
kylianmarceau <kylianmarceau@gmail.com>
2025-08-11 16:39:09 +0200

    create structure for programe to profile serial

---
+++ SerialProfiler.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class SerialProfiler {
+    static List<Integer> GRID_SIZES = Arrays.asList(50, 100, 150, 200);
+    static List<Double> DENSITIES  = Arrays.asList(0.05, 0.10, 0.20);
+    static List<Integer> SEEDS = Arrays.asList(1, 2, 3);
+    public static void main(String[] args) {
+        
+    }
+}
